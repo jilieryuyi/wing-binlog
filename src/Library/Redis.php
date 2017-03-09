87c014166756651c7e62308d24d8dd51c5b89203
xiaoan <huangxiaoan@yonglibao.com>
2017-03-10 06:57:13 +0800

    update .gitignore

--- src/Library/Redis.php
+++ src/Library/Redis.php
@@ -54,6 +54,8 @@ class Redis implements RedisInterface
         try {
             return call_user_func_array([$this->redis, $name], $arguments);
         } catch (\Exception $e) {
+            echo $name,"=>",var_dump($arguments);
+            echo "\r\n";
             var_dump($e->getMessage());
             $this->connect();
         }
