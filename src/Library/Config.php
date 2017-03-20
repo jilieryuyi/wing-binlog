18b06406eb8bc4b3952da6de42ac5a0bacf396f9
xiaoan <huangxiaoan@yonglibao.com>
2017-03-21 07:35:42 +0800

    群集-beta

---
+++ src/Library/Config.php
@@ -0,0 +1,74 @@
+<?php namespace Seals\Library;
+/**
+ * Created by PhpStorm.
+ * User: yuyi
+ * Date: 17/3/21
+ * Time: 07:26
+ */
+class Config
+{
+    private $raw;
+    public function __construct($data)
+    {
+        $this->raw = $data;
+    }
+
+    public function __get($name)
+    {
+        if (is_array($this->raw)) {
+            if(isset($this->raw[$name])) {
+                if (is_array($this->raw[$name]))
+                    return new self($this->raw[$name]);
+                else
+                    return $this->raw[$name];
+            } else {
+                return null;
+            }
+        } elseif (is_object($this->raw)) {
+            $class = new \ReflectionClass($this->raw);
+            if ($class->hasProperty($name))
+                $data = $class->getProperty($name);
+            else
+                $data = null;
+            unset($class);
+            return $data;
+        } else {
+            return $this->raw;
+        }
+    }
+
+    public function get($name)
+    {
+        return $this->__get($name);
+    }
+
+    public function __destruct()
+    {
+        unset($this->raw);
+    }
+
+    /**
+     * @return array
+     */
+    public function toArray()
+    {
+        if (is_array($this->raw))
+            return $this->raw;
+
+        elseif (is_object($this->raw)) {
+            $res         = [];
+            $class       = new \ReflectionClass($this->raw);
+            $properties  = $class->getProperties();
+
+            foreach ($properties as $property) {
+                $key   = $property->getName();
+                $value = $property->getValue();
+                $res[$key] = $value;
+            }
+
+            return $res;
+        } else {
+            return [$this->raw];
+        }
+    }
+}
