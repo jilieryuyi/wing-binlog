9f555f855396129c7d820ac61644465f80257a73
xiaoan <huangxiaoan@yonglibao.com>
2017-03-30 06:53:33 +0800

    离线缓存更新

--- web/js/wing.js
+++ web/js/wing.js
@@ -33,4 +33,19 @@ if (typeof Wing.Error == "undefined") {
 }
 
 
+window.addEventListener('load', function(e) {
+    window.applicationCache.addEventListener('updateready', function(e) {
+        if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+            // Browser downloaded a new app cache.
+            // Swap it in and reload the page to get the new hotness.
+            window.applicationCache.swapCache();
+            //if (confirm('A new version of this site is available. Load it?')) {
+            window.location.reload();
+            //}
+        } else {
+            // Manifest didn't changed. Nothing new to server.
+        }
+    }, false);
+}, false);
+
 
