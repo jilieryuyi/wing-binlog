d308867b292264e183e77784265f459255b26435
xiaoan <huangxiaoan@yonglibao.com>
2017-03-29 20:47:26 +0800

    增加离线缓存

--- web/include/nav.php
+++ web/include/nav.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" manifest="cache.manifest">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <!-- Meta, title, CSS, favicons, etc. -->
