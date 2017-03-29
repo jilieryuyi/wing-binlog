1ee287a804c38af1cc447f67a80971343f5d09b8
xiaoan <huangxiaoan@yonglibao.com>
2017-03-29 21:46:22 +0800

    修复linux信号兼容问题

--- src/Library/Master.php
+++ src/Library/Master.php
@@ -636,7 +636,11 @@ class Master implements Process
                         continue;
                     }
                 }
+                $content = ob_get_contents();
                 ob_end_clean();
+
+                echo $content;
+                unset($content);
             } catch (\Exception $e) {
                 Context::instance()->logger->error($e->getMessage());
             }
