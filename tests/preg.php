0d46a9e721578fbe97cda5e6e58bb9e9247dfec9
xiaoan <huangxiaoan@yonglibao.com>
2017-03-29 20:34:09 +0800

    test

--- tests/preg.php
+++ tests/preg.php
@@ -6,5 +6,5 @@
  * Time: 18:05
  */
 $str = '1232435243<div class="insert-wrap"><iframe class="orderTicket" src="https://v.qq.com/iframe/player.html?vid=g0378faiwpm&amp;tiny=0&amp;auto=0&quot;frameborder=&quot;0&quot;" width="100%" height="400px"></iframe><span class="insert-title">视频描述</span></div>34252345234';
-preg_match_all("/\<div[\s]+class=\"insert-wrap\"\>\<iframe[\s\S]{1,}?\<\/div\>/", $str, $m);
+preg_match_all("|\<div[\s]+class=\"insert-wrap\"\>\<iframe[\s\S]{1,}?\<\/div\>|", $str, $m);
 var_dump($m);
