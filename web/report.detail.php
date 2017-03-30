d8bfef10bd952c837196c2fba01d1d6ef0225e07
xiaoan <huangxiaoan@yonglibao.com>
2017-03-30 17:15:09 +0800

    优化

--- web/report.detail.php
+++ web/report.detail.php
@@ -7,7 +7,7 @@ include "include/nav.php";
         <div class="right_col" role="main">
           <div class="">
             <div class="row top_tiles">
-              <div class="animated flipInY col-lg-3 col-md-4 col-sm-6 col-xs-12">
+              <div class="animated flipInY col-lg-4 col-md-4 col-sm-6 col-xs-12">
                 <div class="tile-stats">
                   <div class="icon"><i class="fa fa-caret-square-o-right"></i></div>
                   <div class="count"><?php echo
@@ -17,7 +17,7 @@ include "include/nav.php";
                   <p>Today insert rows</p>
                 </div>
               </div>
-              <div class="animated flipInY col-lg-3 col-md-4 col-sm-6 col-xs-12">
+              <div class="animated flipInY col-lg-4 col-md-4 col-sm-6 col-xs-12">
                 <div class="tile-stats">
                   <div class="icon"><i class="fa fa-comments-o"></i></div>
                   <div class="count"><?php echo
@@ -27,7 +27,7 @@ include "include/nav.php";
                   <p>Today delete rows</p>
                 </div>
               </div>
-              <div class="animated flipInY col-lg-3 col-md-4 col-sm-6 col-xs-12">
+              <div class="animated flipInY col-lg-4 col-md-4 col-sm-6 col-xs-12">
                 <div class="tile-stats">
                   <div class="icon"><i class="fa fa-sort-amount-desc"></i></div>
                   <div class="count"><?php echo
