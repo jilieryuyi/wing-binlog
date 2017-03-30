6a15d0842b141da0e274a8a19689d50eb0afca48
xiaoan <huangxiaoan@yonglibao.com>
2017-03-30 10:29:34 +0800

    add command

---
+++ src/Console/Command/Role.php
@@ -0,0 +1,39 @@
+<?php namespace Seals\Console\Command;
+
+use Seals\Cache\File;
+use Seals\Library\Master;
+use Seals\Web\Logic\User;
+use Seals\Web\Route;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Command\Command;
+
+class Role extends Command
+{
+    protected function configure()
+    {
+        $this
+            ->setName('role:admin')
+            ->addOption("name", null, InputOption::VALUE_REQUIRED, "角色名称")
+            ->setDescription('将角色设置为全部权限');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $name     = $input->getOption("name");
+
+        $name     = trim($name);
+
+        if (!$name) {
+            echo "params error\r\n";
+            exit;
+        }
+
+        $pages = Route::getAll();
+
+
+        User::roleAdd($name, $pages);
+
+    }
+}
