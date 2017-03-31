c71e9fcc84e3092f9d4664409919f2d91279c7a3
xiaoan <huangxiaoan@yonglibao.com>
2017-03-31 13:30:29 +0800

    优化

--- tests/mysql.php
+++ tests/mysql.php
@@ -5,9 +5,10 @@
  * Date: 17/3/31
  * Time: 11:02
  */
-$handle = popen("ps aux | grep mysqld","r");
-$res = fgets($handle, 1024);
+define("__APP_DIR__", dirname(__DIR__));
+include __DIR__."/../vendor/autoload.php";
 
-echo $res;
+$command = new \Seals\Library\Command("ps aux | grep mysqld");
+$res = $command->run();
 
-pclose($handle);
+echo $res,"\r\n";
