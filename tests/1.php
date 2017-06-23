2d88679897b1f0f054191b9f66993d390eb8952f
huangxiaoan <huangxiaoan@xunlei.com>
2017-06-23 07:50:59 +0100

    test

---
+++ tests/1.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2017/6/23
+ * Time: 14:43
+ */
+
+echo dechex(22);
--- tests/str.php
+++ tests/str.php
@@ -20,24 +20,196 @@ define('FCGI_STDIN', 5);
 define('FCGI_STDOUT', 6);
 define('FCGI_STDERR', 7);
 
-echo strlen(pack("nC6", FCGI_RESPONDER, 0, 0, 0, 0, 0, 0)),"\r\n";
+
+function getBeginRequestBody()
+{
+	return pack("nC6", FCGI_RESPONDER, 0, 0, 0, 0, 0, 0);
+}
+
+function getHeader($type, $requestId, $contentLength, $paddingLength, $reserved=0)
+{
+	return pack("C2n2C2", FCGI_VERSION_1, $type, $requestId, $contentLength, $paddingLength, $reserved);
+}
+
+function getPaddingLength($body)
+{
+	$left = strlen($body) % 8;
+	if ($left == 0)
+	{
+		return 0;
+	}
+
+	return (8 - $left);
+}
+
+function getPaddingData($paddingLength=0)
+{
+	if ($paddingLength <= 0)
+	{
+		return '';
+	}
+	$paddingArray = array_fill(0, $paddingLength, 0);
+	return call_user_func_array("pack", array_merge(array("C{$paddingLength}"), $paddingArray));
+}
+
+function getNameValue($name, $value)
+{
+	$nameLen  = strlen($name);
+	$valueLen = strlen($value);
+	$bin      = '';
+
+	// 如果大于127，则需要4个字节来存储，下面的$valueLen也需要如此计算
+	if ($nameLen > 0x7f)
+	{
+		// 将$nameLen变成4个无符号字节
+		$b0 = $nameLen << 24;
+		$b1 = ($nameLen << 16) >> 8;
+		$b2 = ($nameLen << 8) >> 16;
+		$b3 = $nameLen >> 24;
+		// 将最高位置1，表示采用4个无符号字节表示
+		$b3 = $b3 | 0x80;
+		$bin = pack("C4", $b3, $b2, $b1, $b0);
+	}
+	else
+	{
+		$bin = pack("C", $nameLen);
+	}
+
+	if ($valueLen > 0x7f)
+	{
+		// 将$nameLen变成4个无符号字节
+		$b0 = $valueLen << 24;
+		$b1 = ($valueLen << 16) >> 8;
+		$b2 = ($valueLen << 8) >> 16;
+		$b3 = $valueLen >> 24;
+		// 将最高位置1，表示采用4个无符号字节表示
+		$b3 = $b3 | 0x80;
+		$bin .= pack("C4", $b3, $b2, $b1, $b0);
+	}
+	else
+	{
+		$bin .= pack("C", $valueLen);
+	}
+
+	$bin .= pack("a{$nameLen}a{$valueLen}", $name, $value);
+
+	return $bin;
+}
+
+//$sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+//socket_connect($sock, FCGI_HOST, FCGI_PORT);
+
+$body   = getBeginRequestBody();
+$paddingLength = getPaddingLength($body);
+$header = getHeader(FCGI_BEGIN_REQUEST, FCGI_REQUEST_ID, strlen($body), $paddingLength, 0);
+$record = $header . $body . getPaddingData($paddingLength);
+
+//echo "header length : ".strlen($record),"\r\n";
+
+
+//echo strlen(pack("nC6", FCGI_RESPONDER, 0, 0, 0, 0, 0, 0)),"\r\n";
 
 $data = file_get_contents("D:/123.log");
 $headerFormat = 'Cversion/Ctype/nrequestId/ncontentLength/CpaddingLength/x';
 
-$arr = unpack($headerFormat, substr($data,0,8));
 
-var_dump($arr);
 
-$arr = unpack("nC6", substr($data,9,8));
-var_dump($arr);
+//第一个包
+//$arr = unpack($headerFormat, substr($data,17,8));
+//var_dump($arr);
+/*$arr1 = unpack("nC6", substr($data,9,$arr["contentLength"]));
+var_dump($arr1);
 
-$arr = unpack($headerFormat, substr($data,17,8));
-var_dump($arr);
+$arr2 = unpack($headerFormat, substr($data,9+$arr["contentLength"],8));
+var_dump($arr2);*/
 
-$arr = unpack("C4", substr($data,25));
-var_dump($arr);
-var_dump($arr[1]>>24);
-var_dump(($arr[2]>>16)<<8);
-var_dump(($arr[3]>>8)<<16);
-var_dump($arr[4]>>24);
+
+//echo "name===>";
+//$start = 18;
+//$f = substr($data,$start , 1);
+//var_dump("namelen flag ====>",$f);
+//
+//
+//for($i=0;$i<30;$i++) {
+//	$arr = unpack("C", substr($data, $i));
+//	echo $i,"=>length ===> " . $arr[1], "\r\n";
+//}
+
+
+
+$start = 24;
+$length = strlen($data);
+
+//while ($start < $length)
+{
+	$f = substr($data, $start, 1);
+	$temp = dechex(ord($f));
+
+	$temp = strlen($temp)<2? "0".$temp:$temp;
+	$flag = substr($temp,0,1);
+	if ($flag == "0") {
+		//echo "=================>";
+		$temp = unpack("C", substr($data, $start, 1));
+		//var_dump($temp);
+		$name_len = unpack("C", substr($data, $start, 1))[1];
+		$start += 1;
+	} else {
+		$temp  = unpack("C4", substr($data, $start, 4));
+		$B3 = $temp[1];
+		$B2 = $temp[2];
+		$B1 = $temp[3];
+		$B0 = $temp[4];
+		$name_len = (($B3 & 0x7f) << 24) + ($B2 << 16) + ($B1 << 8) + $B0;
+		$start +=4;
+	}
+
+	echo $name_len,"--->";
+
+	$key = substr($data, $start, $name_len+1);
+
+
+	$f    = substr($data, $start, 1);
+	$temp = dechex(ord($f));
+
+	$temp = strlen($temp)<2? "0".$temp:$temp;
+	$flag = substr($temp,0,1);
+	if ($flag == "0") {
+	//if (!$f) {
+		$value_len = unpack("C", substr($data, $start, 1))[1];
+		$start += 1;
+	} else {
+		$temp  = unpack("C4", substr($data, $start, 4));
+		$B3 = $temp[1];
+		$B2 = $temp[2];
+		$B1 = $temp[3];
+		$B0 = $temp[4];
+		$value_len = (($B3 & 0x7f) << 24) + ($B2 << 16) + ($B1 << 8) + $B0;
+		$start +=4;
+	}
+	echo $value_len,"\r\n";
+
+	$start += $name_len;
+	$value = substr($data, $start, $value_len+1);
+	$start += $value_len;
+
+	//echo $key ,"===>" , $value,"\r\n";
+	exit;
+}
+
+
+
+/*$f = substr($data,9+$arr["contentLength"]+9 , 1);
+var_dump("content flag ====>",$f);
+
+if ($f == 0) {
+	$arr = unpack("C", substr($data,9+$arr["contentLength"]+9));
+	var_dump($arr);
+}*/
+
+
+
+//((B3 & 0x7f) << 24) + (B2 << 16) + (B1 << 8) + B0
+//var_dump($arr[1]>>24);
+//var_dump(($arr[2]>>16)<<8);
+//var_dump(($arr[3]>>8)<<16);
+//var_dump($arr[4]>>24);
