0a8c0ea0ac446c659ba3f2a972349ce3ec82cbd7
xiaoan <huangxiaoan@yonglibao.com>
2017-08-05 19:07:45 +0800

    dev

--- src/Library/ParseWorker.php
+++ src/Library/ParseWorker.php
@@ -80,7 +80,7 @@ class ParseWorker extends BaseWorker
                                 "table_name"    => $table_name,
                                 "event_data"    => $event,
                             ];
-                            var_dump($params);
+                            //var_dump($params);
                             $websocket->onchange($database_name, $table_name, $event);
                             $tcp->onchange($database_name, $table_name, $event);
 
