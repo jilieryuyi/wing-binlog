2be5be41fdb177ae8f17ad950315f3d11add550a
xiaoan <huangxiaoan@yonglibao.com>
2017-08-06 21:00:23 +0800

    test

--- tests/pdo.php
+++ tests/pdo.php
@@ -15,11 +15,11 @@ $sql2 = 'update new_yonglibao_c.bl_city set provinces_id=(provinces_id-1) where 
 //$sql1 = 'update xsl.x_messages set phone=(phone+1) where 1';
 //$sql2 = 'update xsl.x_messages set phone=(phone-1) where 1';
 $count = 0;
-while ($count<10000) {
+while (1) {//$count<10000
     $count +=2;
     $pdo->query($sql1);
     $pdo->query($sql2);
     echo "事件次数：",$count,"\r\n";
-    //usleep(1000);
+    usleep(1000);
 }
 
