eeecfc43a44b378c1fb14516de52ad5e24f1ebe7
huangxiaoan <huangxiaoan@xunlei.com>
2017-08-08 11:43:43 +0800

    show version

--- src/Command/ServerVersion.php
+++ src/Command/ServerVersion.php
@@ -1,8 +1,9 @@
-<?php namespace Seals\Console\Command;
+<?php namespace Wing\Command;
 
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Wing\Library\Worker;
 
 class ServerVersion extends ServerBase
 {
@@ -18,7 +19,7 @@ class ServerVersion extends ServerBase
     protected function execute(InputInterface $input, OutputInterface $output)
     {
 
-        echo "wing-binlog版本号 : ",$this->version(),"\r\n";
+        echo "wing-binlog版本号 : ",Worker::VERSION,"\r\n";
         echo "作者 : yuyi\r\n";
         echo "邮箱 : 297341015@qq.com\r\n";
         echo "QQ群 : 535218312\r\n";
