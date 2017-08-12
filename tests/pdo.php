93d1097e077646a64cb90c2f409bfe583f39e546
xiaoan <huangxiaoan@yonglibao.com>
2017-08-12 10:30:21 +0800

    dev

--- tests/pdo.php
+++ tests/pdo.php
@@ -16,7 +16,7 @@ $sql2 = 'update new_yonglibao_c.bl_city set provinces_id=(provinces_id-1) where 
 $sql1 = 'update xsl.x_messages set phone=(phone+1) where id=3';
 $sql2 = 'update xsl.x_messages set phone=(phone-1) where id=3';
 $count = 0;
-//while ($count<100000)
+while ($count<1000000)
 {//$count<10000
     $count +=2;
     ($pdo->query($sql1));
