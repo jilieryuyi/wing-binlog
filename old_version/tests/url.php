e392ebd498506cc2fe534378547f40a1c0db46da
xiaoan <huangxiaoan@yonglibao.com>
2017-08-12 17:54:46 +0800

    移除老版本

--- old_version/tests/url.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: yuyi
- * Date: 17/3/16
- * Time: 15:56
- */
-var_dump(parse("group_id=wing-binlog&session_id=1489647717-4937e12016f3782586e0d7ebdc045f363d977a9bc09507eb"));
