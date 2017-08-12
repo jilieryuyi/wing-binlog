e49cfcb2284d262b098a8bcd8c0b0eb3578e5ba3
xiaoan <huangxiaoan@yonglibao.com>
2017-08-12 09:21:48 +0800

    dev

--- tests/pdo.php
+++ tests/pdo.php
@@ -12,15 +12,15 @@ $pdo = new \Wing\Library\PDO();
 $sql1 = 'update new_yonglibao_c.bl_city set provinces_id=(provinces_id+1) where id=5753598';
 $sql2 = 'update new_yonglibao_c.bl_city set provinces_id=(provinces_id-1) where id=5753598';
 
-$sql1 = 'update xsl.x_messages set phone=(phone+1) where 1';
-$sql2 = 'update xsl.x_messages set phone=(phone-1) where 1';
+$sql1 = 'update xsl.x_messages set phone=(phone+1) where id=3';
+$sql2 = 'update xsl.x_messages set phone=(phone-1) where id=3';
 $count = 0;
-//while ($count<1000)
+while ($count<1000)
 {//$count<10000
     $count +=2;
-    $pdo->query($sql1);
-    $pdo->query($sql2);
-    echo "事件次数：",$count,"\r\n";
-    usleep(1000);
+    var_dump($pdo->query($sql1));
+    var_dump($pdo->query($sql2));
+//    echo "事件次数：",$count,"\r\n";
+//    usleep(1000);
 }
 
