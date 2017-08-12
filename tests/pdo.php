0e8e91090ef9b8e9f51dd3fd2de6701904287573
xiaoan <huangxiaoan@yonglibao.com>
2017-08-12 10:26:34 +0800

    dev

--- tests/pdo.php
+++ tests/pdo.php
@@ -7,6 +7,7 @@
  */
 include __DIR__."/../vendor/autoload.php";
 define("HOME", dirname(__DIR__));
+define("WING_DEBUG",  true);
 
 $pdo = new \Wing\Library\PDO();
 $sql1 = 'update new_yonglibao_c.bl_city set provinces_id=(provinces_id+1) where id=5753598';
@@ -15,7 +16,7 @@ $sql2 = 'update new_yonglibao_c.bl_city set provinces_id=(provinces_id-1) where 
 $sql1 = 'update xsl.x_messages set phone=(phone+1) where id=3';
 $sql2 = 'update xsl.x_messages set phone=(phone-1) where id=3';
 $count = 0;
-while ($count<100000)
+//while ($count<100000)
 {//$count<10000
     $count +=2;
     ($pdo->query($sql1));
