fdeb5c30c91be3c061df4409bff337d711e74d3a
xiaoan <huangxiaoan@yonglibao.com>
2017-08-12 15:07:42 +0800

    dev

--- src/Command/ServerStop.php
+++ src/Command/ServerStop.php
@@ -17,8 +17,8 @@ class ServerStop extends ServerBase
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-    	exec("php ".HOME."/tcp stop");
-		exec("php ".HOME."/websocket stop");
+    	exec("php ".HOME."/services/tcp stop");
+		exec("php ".HOME."/services/websocket stop");
         Worker::stopAll();
     }
 }
