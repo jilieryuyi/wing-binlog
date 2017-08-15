fcfce0cff3e9cd0f4cede45052a07f1f3d88d61f
huangxiaoan <huangxiaoan@xunlei.com>
2017-08-15 13:59:46 +0800

    test

--- tests/status.php
+++ tests/status.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 /**
  * Created by PhpStorm.
@@ -18,27 +17,18 @@ var_dump($output, $return);
 $start = false;
 $processes = [];
 foreach ($output as $row) {
-    if (substr($row, 0, 3) == "pid") {
-        $start = true;
-        continue;
-    }
-    if ($start) {
-        list($process_id,) = preg_split("/\D/", $row, 2);
-        $processes[]= $process_id;//,"\r\n";
-    }
+	if (substr($row, 0, 3) == "pid") {
+		$start = true;
+		continue;
+	}
+	if ($start) {
+		list($process_id,) = preg_split("/\D/", $row, 2);
+		$processes[] = $process_id;//,"\r\n";
+	}
 }
-var_dump($processes);
-=======
-<?php
-/**
- * Created by PhpStorm.
- * User: yuyi
- * Date: 17/8/7
- * Time: 07:20
- */
-include_once __DIR__."/../vendor/autoload.php";
-define("HOME", dirname(__DIR__));
+
+
 
 $client = new \Wing\Net\WsClient("127.0.0.1", 9998, "/");
 $client->send('hello');
->>>>>>> 6ee3cbd6544d951ff92c5114316e3e698587ea1a
+
