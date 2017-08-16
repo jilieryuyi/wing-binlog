a0cc5a032346389aae48ca60cfc8138da785bde0
xiaoan <huangxiaoan@yonglibao.com>
2017-08-16 14:43:04 +0800

    dev

--- tests/websocket.php
+++ tests/websocket.php
@@ -8,6 +8,11 @@
 include_once __DIR__."/../vendor/autoload.php";
 define("HOME", dirname(__DIR__));
 
-//$websocket = new \Wing\Library\WebSocketWorker();
-//$websocket->start(false);
+$client = new \Wing\Net\WsClient("127.0.0.1", 9990, "/");
+while (1) {
+    $client->send(json_encode([
+        "event_index"=>0,
+        "event"=>1
+    ])."\r\n\r\n\r\n");
+}
 
