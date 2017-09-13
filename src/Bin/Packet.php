e0cdccda52640945c12ccd58fe6271ae953b1cdf
huangxiaoan <huangxiaoan@xunlei.com>
2017-09-13 18:30:11 +0800

    test

--- src/Bin/Packet.php
+++ src/Bin/Packet.php
@@ -35,39 +35,32 @@ class Packet
 	 */
 	public static function  getAuthPack($flag, $user, $pass, $salt, $db = '')
 	{
+		$data 	= pack('L',$flag);						 	//4bytes权能信息
+		$data  .= pack('L', self::PACK_MAX_LENGTH); 	//4bytes最大长度
+		$data  .= chr(MysqlChartSet::utf8_general_ci);			//1byte字符编码
 
-		$data = pack('L',$flag);
-
-		// max-length 4bytes，最大16M 占3bytes
-		$data .= pack('L', self::PACK_MAX_LENGTH);
-
-
-		// Charset  1byte utf8=>33
-		$data .= chr(MysqlChartSet::utf8_general_ci);
-
-
-		// 空 bytes23
+		//填充23字节0x00
 		for ($i = 0; $i < 23; $i++) {
-			$data .=chr(0);
+			$data .= chr(0);
 		}
 
-		// http://dev.mysql.com/doc/internals/en/secure-password-authentication.html#packet-Authentication::Native41
-		$result = sha1($pass, true) ^ sha1($salt . sha1(sha1($pass, true), true),true);
+		//用户名 0x00 以NULL结束
+		$data   .= $user . chr(0) ;
+		//密码加密
+		$result  = sha1($pass, true) ^ sha1($salt . sha1(sha1($pass, true), true),true);
+		//密码信息 Length Coded Binary
+		$data 	.= chr(strlen($result)) . $result;
 
-		//转码 8是 latin1
-		//$user = iconv('utf8', 'latin1', $user);
-
-		//
-		$data = $data . $user . chr(0) . chr(strlen($result)) . $result;
+		//数据库名称  0x00 以NULL结束
 		if ($db) {
 			$data .= $db . chr(0);
 		}
 
-		// V L 小端，little endian
-		$str = pack("L", strlen($data));
-		$s = $str[0].$str[1].$str[2];
-
-		$data = $s . chr(1) . $data;
+		$str  = pack("L", strlen($data));
+		//报文结构生成
+		//$str[0].$str[1].$str[2] 为消息长度 chr(1)为序号信息必须为1 $data 部分为消息体
+		//$str[0].$str[1].$str[2] . chr(1) 占4bytes 为消息头
+		$data = $str[0].$str[1].$str[2] . chr(1) . $data;
 
 		return $data;
 	}
