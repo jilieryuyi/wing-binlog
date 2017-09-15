be8f8b6bf5453e3cc7d0a4c937bee91e76aa5d0d
xiaoan <huangxiaoan@yonglibao.com>
2017-09-15 20:07:36 +0800

    完善注释

--- tests/mysql-client/query.php
+++ tests/mysql-client/query.php
@@ -21,21 +21,21 @@ define("WING_DEBUG", true);
 $start = time();
 
 try {
-	$mysql_config = load_config("app");
-	$context = new \Wing\Bin\Context();
-	$pdo = new \Wing\Library\PDO();
+	$mysql_config   = load_config("app");
+	$context        = new \Wing\Bin\Context();
+	$pdo            = new \Wing\Library\PDO();
 
-	$context->pdo = \Wing\Bin\Db::$pdo = $pdo;
-	$context->host = $mysql_config["mysql"]["host"];
-	$context->db_name = $mysql_config["mysql"]["db_name"];
-	$context->user = $mysql_config["mysql"]["user"];
-	$context->password = $mysql_config["mysql"]["password"];
-	$context->port = $mysql_config["mysql"]["port"];
-	$context->checksum = !!\Wing\Bin\Db::getChecksum();
+	$context->pdo       = \Wing\Bin\Db::$pdo = $pdo;
+	$context->host      = $mysql_config["mysql"]["host"];
+	$context->db_name   = $mysql_config["mysql"]["db_name"];
+	$context->user      = $mysql_config["mysql"]["user"];
+	$context->password  = $mysql_config["mysql"]["password"];
+	$context->port      = $mysql_config["mysql"]["port"];
+	$context->checksum  = !!\Wing\Bin\Db::getChecksum();
 
-	$context->slave_server_id = $mysql_config["slave_server_id"];
-	$context->last_binlog_file = null;
-	$context->last_pos = 0;
+	$context->slave_server_id   = $mysql_config["slave_server_id"];
+	$context->last_binlog_file  = null;
+	$context->last_pos          = 0;
 
     //认证
 	\Wing\Bin\Auth\Auth::execute($context);
