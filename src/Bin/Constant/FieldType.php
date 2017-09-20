ec9b64862da0c0b2cfddf32d97e5bc0fd4a812f0
xiaoan <huangxiaoan@yonglibao.com>
2017-09-21 00:00:11 +0800

    field type 更新

--- src/Bin/Constant/FieldType.php
+++ src/Bin/Constant/FieldType.php
@@ -46,39 +46,39 @@ class FieldType
     const   INTERVAL 	= self::ENUM;
     const   JSON        = 245;*/
 //libmysql/libmysql.c 2933
-const DECIMAL = 0;
+const DECIMAL       = 0;
 
-const TINY = 1; //1 byte store_param_tinyint
-const SHORT = 2; //2 bytes store_param_short
-const LONG = 3; //4 bytes store_param_int32
-const FLOAT = 4; //4 bytes store_param_float
-const DOUBLE = 5; // 8bytes store_param_double
-const NULL = 6; //int_is_null_true == 1
-const TIMESTAMP = 7; //store_param_datetime
+const TINY          = 1; //1 byte store_param_tinyint
+const SHORT         = 2; //2 bytes store_param_short
+const LONG          = 3; //4 bytes store_param_int32
+const FLOAT         = 4; //4 bytes store_param_float
+const DOUBLE        = 5; // 8bytes store_param_double
+const NULL          = 6; //int_is_null_true == 1
+const TIMESTAMP     = 7; //store_param_datetime
 
-const LONGLONG = 8; //8 bytes store_param_int64
-const BIGINT = 8; //alias LONGLONG 8 bytes store_param_int64
+const LONGLONG      = 8; //8 bytes store_param_int64
+const BIGINT        = 8; //alias LONGLONG 8 bytes store_param_int64
 
-const INT24 = 9;
-const DATE = 10;
-const TIME = 11;
-const DATETIME = 12;
-const YEAR = 13;
-const NEWDATE = 14;
-const VARCHAR = 15;//store_param_str
-const BIT = 15;
-const TIMESTAMP2 = 17;
-const DATETIME2 = 18;
-const TIME2 = 19;
-const JSON=245;//store_param_str
-const NEWDECIMAL=246;//store_param_str
-const ENUM=247;
-const SET=248;
-const TINY_BLOB=249;//store_param_str
-const MEDIUM_BLOB=250;//store_param_str
-const LONG_BLOB=251;//store_param_str
-const BLOB=252;//store_param_str
-const VAR_STRING=253;//store_param_str
-const STRING=254;//store_param_str
-const GEOMETRY=255;
+const INT24         = 9;
+const DATE          = 10;
+const TIME          = 11;
+const DATETIME      = 12;
+const YEAR          = 13;
+const NEWDATE       = 14;
+const VARCHAR       = 15;//store_param_str
+const BIT           = 16;
+const TIMESTAMP2    = 17;
+const DATETIME2     = 18;
+const TIME2         = 19;
+const JSON          = 245;//store_param_str
+const NEWDECIMAL    = 246;//store_param_str (new in MYSQL 5.0)
+const ENUM          = 247;
+const SET           = 248;
+const TINY_BLOB     = 249;//store_param_str
+const MEDIUM_BLOB   = 250;//store_param_str
+const LONG_BLOB     = 251;//store_param_str
+const BLOB          = 252;//store_param_str
+const VAR_STRING    = 253;//store_param_str
+const STRING        = 254;//store_param_str
+const GEOMETRY      = 255;
 }
