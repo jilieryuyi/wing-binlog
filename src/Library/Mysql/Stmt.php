090619831935e908bdb6174d0f5f44cbaaa6214b
xiaoan <huangxiaoan@yonglibao.com>
2017-10-18 08:01:27 +0800

    stmt 支持

--- src/Library/Mysql/Stmt.php
+++ src/Library/Mysql/Stmt.php
@@ -7,20 +7,48 @@
  */
 class Stmt
 {
-public $current_field;//(){}//Get current field offset of a result pointer
-    public $field_count;//(){}//Get the number of fields in a result
-    public $lengths;//(){}//Returns the lengths of the columns of the current row in the result set
-    public $num_rows;//(){}//Gets the number of rows in a result
+	//Get current field offset of a result pointer
+	public $current_field;
 
-    public function data_seek(){}//Adjusts the result pointer to an arbitrary row in the result
-public function fetch_all(){}//Fetches all result rows as an associative array, a numeric array, or both
-public function fetch_array(){}//Fetch a result row as an associative, a numeric array, or both
-public function fetch_assoc(){}//Fetch a result row as an associative array
-public function fetch_field_direct(){}//Fetch meta-data for a single field
-public function fetch_field(){}//Returns the next field in the result set
-public function fetch_fields(){}//Returns an array of objects representing the fields in a result set
-public function fetch_object(){}//Returns the current row of a result set as an object
-public function fetch_row(){}//Get a result row as an enumerated array
-public function field_seek(){}//Set result pointer to a specified field offset
-public function free(){}//Frees the memory associated with a result
+	//Get the number of fields in a result
+	public $field_count;
+
+	//Returns the lengths of the columns of the current row in the result set
+	public $lengths;
+
+	//Gets the number of rows in a result
+	public $num_rows;
+
+	//Adjusts the result pointer to an arbitrary row in the result
+	public function data_seek(){}
+
+	//Fetches all result rows as an associative array, a numeric array, or both
+	public function fetch_all(){}
+
+	//Fetch a result row as an associative, a numeric array, or both
+	public function fetch_array(){}
+
+	//Fetch a result row as an associative array
+	public function fetch_assoc(){}
+
+	//Fetch meta-data for a single field
+	public function fetch_field_direct(){}
+
+	//Returns the next field in the result set
+	public function fetch_field(){}
+
+	//Returns an array of objects representing the fields in a result set
+	public function fetch_fields(){}
+
+	//Returns the current row of a result set as an object
+	public function fetch_object(){}
+
+	//Get a result row as an enumerated array
+	public function fetch_row(){}
+
+	//Set result pointer to a specified field offset
+	public function field_seek(){}
+
+	//Frees the memory associated with a result
+	public function free(){}
 }
