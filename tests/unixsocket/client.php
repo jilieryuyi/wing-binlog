1305adcc63cc2e995a944a02bf17555acae551f2
xiaoan <huangxiaoan@yonglibao.com>
2017-10-22 07:20:10 +0800

    unix udp socket

--- tests/unixsocket/client.php
+++ tests/unixsocket/client.php
@@ -6,13 +6,29 @@
  * Time: 16:08
  */
 
-/* create a socket */
+/*
 $socket = socket_create(AF_UNIX, SOCK_STREAM, 0);
 $sp     = __DIR__."/unix_socket.socket";
 $msg    = "hello";
 
 socket_connect($socket, $sp);
-socket_write($socket, $msg, strlen($msg));
+while(1) {
+	socket_write($socket, $msg, strlen($msg));
+}
 
 echo socket_read($socket, 10240);
+socket_close($socket);
+*/
+
+$socket = socket_create(AF_UNIX, SOCK_DGRAM, 0);
+$sp     = __DIR__."/unix_socket.socket";
+$msg    = "hello";
+
+socket_connect($socket, $sp);
+
+while (1) {
+	//socket_sendto($socket, $msg, strlen($msg),0, $sp);
+	socket_write($socket, $msg, strlen($msg));
+}
+
 socket_close($socket);
--- tests/unixsocket/server.php
+++ tests/unixsocket/server.php
@@ -5,15 +5,42 @@
  * Date: 17/10/21
  * Time: 15:53
  */
-
+/*
 $socket = socket_create(AF_UNIX, SOCK_STREAM, IPPROTO_IP);
 $sp     = __DIR__."/unix_socket.socket";
+
+register_shutdown_function(function() use ($sp){
+	unlink($sp);
+});
+
 $bind   = socket_bind($socket, $sp);
 $listen = socket_listen($socket);
 $client = socket_accept($socket);
 $msg    = "welcome to unix socket";
 
-echo socket_read($client, 10240);
+while ($recv = socket_read($client, 10240)) {
+	echo $recv,"\r\n";
+}
+//echo socket_read($client, 10240);
 
 socket_write($client, $msg, strlen($msg));
 socket_close($socket);
+*/
+
+$socket = socket_create(AF_UNIX, SOCK_DGRAM, IPPROTO_IP);
+$sp     = __DIR__."/unix_socket.socket";
+
+register_shutdown_function(function() use ($sp){
+	unlink($sp);
+});
+
+$bind   = socket_bind($socket, $sp);
+$msg    = "welcome to unix socket";
+
+while ($n = socket_recvfrom($socket, $recv, 10240, 0, $from)) {
+	echo $recv,"\r\n";
+}
+
+socket_close($socket);
+
+
